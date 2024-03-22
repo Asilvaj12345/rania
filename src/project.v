55feb50b51c4d5f2a08f2e1b6d6320a29756f3a9
Asilvaj12345 <163932021+Asilvaj12345@users.noreply.github.com>
2024-03-22 15:30:23 -0600

    Update project.v

--- src/project.v
+++ src/project.v
@@ -5,7 +5,7 @@
 
 `define default_netname none
 
-module tt_um_example (
+module tt_um_Top (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
@@ -17,8 +17,11 @@ module tt_um_example (
 );
 
   // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
+
+    Top code (.A(ui_in[2:0]), .B(ui_in[5:3]), .ctrl(uio_in[2:0]), .Leds(uo_out[5:0]));
+
+    assign uio_oe = 8'b00111111;
+    assign uo_out[7:6] = 2'b00;
+    assign uio_out[7:0] 8'b00000000;   
 
 endmodule
