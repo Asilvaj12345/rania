c90507218665c2776078fe9cf6783a719ea83c16
Asilvaj12345 <163932021+Asilvaj12345@users.noreply.github.com>
2024-03-22 15:43:14 -0600

    Update ALU.v

--- src/ALU.v
+++ src/ALU.v
@@ -1,6 +1,10 @@
 `timescale 1ns / 1ps
-module ALU (input [2:0]A,B, output reg [3:0]Y1_ADD, output reg[5:0]Y3_MULT, 
-                                  output reg[2:0]Y2_MIN, Y4_DIV, Y5_MOD);
+module ALU (
+  input [2:0]A,B, 
+  output reg [3:0]Y1_ADD, 
+  output reg [5:0]Y3_MULT, 
+  output reg [2:0]Y2_MIN, Y4_DIV, Y5_MOD);
+  
 always@(A,B)
 begin
 Y1_ADD = A + B;
