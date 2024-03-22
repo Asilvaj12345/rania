71237826742bfbbb7a69311ad1dd8c060f4b1725
Asilvaj12345 <163932021+Asilvaj12345@users.noreply.github.com>
2024-03-22 15:38:27 -0600

    Update project.v

--- src/project.v
+++ src/project.v
@@ -5,7 +5,7 @@
 
 `define default_netname none
 
-module tt_um_Top (
+module tt_um_project (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
